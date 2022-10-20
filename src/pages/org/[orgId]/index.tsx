5df54276b4dfa9ef32710529f819c728f040fde1
Mike Alicea <mfalicea58@gmail.com>
2022-10-20 14:32:06 -0400

    Handles joining org when landing on org page

--- src/pages/org/[orgId]/index.tsx
+++ src/pages/org/[orgId]/index.tsx
@@ -1,10 +1,21 @@
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Head from 'next/head';
 import { Amplify } from 'aws-amplify';
 import { getUserOnServer, AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
 // import { useRouter } from 'next/router';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+
+import { useAppDispatch, useAppSelector } from '@/reduxHooks';
+import { useFetchSelf } from '@/shared_hooks/sharedHooks';
+import { selectSelf } from '@/features/selfSlice';
+import {
+  fetchPostOrgFriendlyOrgJoin,
+  fetchPostOrgMemberJoin,
+  selectFriendlyOrgJoinStatus,
+  selectMemberJoinStatus,
+} from '@/features/orgJoinSlice';
 
 // import { useAppSelector, useAppDispatch } from '@/reduxHooks';
 // import { selectSelf, selectSelfStatus } from '@/features/selfSlice';
@@ -14,9 +25,12 @@ import Footer from '@/shared_components/footer/footer';
 // https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
 
-const OrgIndex = () => {
+const OrgIndex = ({ userJwt }: { userJwt: string }) => {
+  console.log('here yea?', userJwt);
   const router = useRouter();
   const { signOut } = useAuthenticator((context) => [context.signOut]);
+  useFetchSelf(userJwt);
+  useHandleJoinOrgs(userJwt);
 
   return (
     <div>
@@ -51,14 +65,84 @@ const OrgIndex = () => {
   );
 };
 
+function useHandleJoinOrgs(userJwt: string | null) {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const self = useAppSelector((state) => selectSelf(state));
+  const friendlyOrgJoinStatus = useAppSelector((state) =>
+    selectFriendlyOrgJoinStatus(state)
+  );
+  const memberJoinStatus = useAppSelector((state) =>
+    selectMemberJoinStatus(state)
+  );
+
+  /**
+   * Users will only have 1 primary org at a time.
+   * But to avoid what I had with v1 Communion, lets not fully assume that
+   * Who knows what the future holds
+   */
+  const primarySelfOrg = self && self.orgs[0];
+
+  const {
+    orgId: queryOrgId,
+    joinCode: queryJoinCode,
+    action: queryAction,
+    role: queryRole,
+  } = router.query;
+  const orgId = (queryOrgId as string) || '';
+  const joinCode = (queryJoinCode as string) || '';
+  const action = (queryAction as string) || '';
+  const role = (queryRole as string) || '';
+
+  useEffect(() => {
+    if (
+      action === 'memberJoin' &&
+      self &&
+      orgId &&
+      joinCode &&
+      memberJoinStatus === 'idle'
+    ) {
+      dispatch(
+        fetchPostOrgMemberJoin({
+          memberId: self.id,
+          orgId,
+          jwtToken: userJwt || '',
+          role,
+        })
+      );
+    } else if (
+      action === 'friendlyOrgJoin' &&
+      orgId &&
+      joinCode &&
+      primarySelfOrg &&
+      friendlyOrgJoinStatus === 'idle'
+    ) {
+      dispatch(
+        fetchPostOrgFriendlyOrgJoin({
+          orgId,
+          friendlyOrgId: primarySelfOrg,
+          jwtToken: userJwt || '',
+        })
+      );
+    }
+  }, [
+    action,
+    dispatch,
+    friendlyOrgJoinStatus,
+    joinCode,
+    memberJoinStatus,
+    orgId,
+    primarySelfOrg,
+    self,
+    userJwt,
+    role,
+  ]);
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const { userJwt, userId } = await getUserOnServer(context);
-    if (userJwt) {
-      return {
-        props: {},
-      };
-    }
     return {
       props: { userJwt, userId },
     };
