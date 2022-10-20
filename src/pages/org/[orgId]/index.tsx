6a2e7f1c685de7656e9b7463fccbf3c7c83ce50c
Mike Alicea <mfalicea58@gmail.com>
2022-10-20 16:53:08 -0400

    Adds some org info

--- src/pages/org/[orgId]/index.tsx
+++ src/pages/org/[orgId]/index.tsx
@@ -6,6 +6,11 @@ import { Amplify } from 'aws-amplify';
 import { getUserOnServer, AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
 // import { useRouter } from 'next/router';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import {
+  BuildingOfficeIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid';
 
 import { useAppDispatch, useAppSelector } from '@/reduxHooks';
 import { useFetchSelf } from '@/shared_hooks/sharedHooks';
@@ -22,6 +27,7 @@ import {
 
 import Footer from '@/shared_components/footer/footer';
 import { fetchOrg, selectOrg, selectOrgStatus } from '@/features/orgSlice';
+import Link from 'next/link';
 
 // https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
@@ -42,7 +48,7 @@ const OrgIndex = ({ userJwt }: { userJwt: string }) => {
   );
 
   useFetchSelf(userJwt);
-  useHandleJoinOrgs(userJwt);
+  useHandleJoinOrg(userJwt);
 
   useEffect(() => {
     const orgId = router.query.orgId as string;
@@ -73,6 +79,110 @@ const OrgIndex = ({ userJwt }: { userJwt: string }) => {
               <JoinInProgress status={memberJoinStatus} />
             )}
           </div>
+
+          {org && (
+            <div>
+              <div>
+                <h2 className="text-xl">{org.name}</h2>
+              </div>
+
+              <>
+                <div className="mt-4">
+                  <h3 className="text-lg">Employees</h3>
+                  <ul
+                    role="list"
+                    className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+                  >
+                    {org.primaryMembers.map((person) => (
+                      <li
+                        key={person}
+                        className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
+                      >
+                        <div className="flex w-full items-center justify-between space-x-6 p-6">
+                          <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                              <h3 className="truncate text-sm font-medium text-gray-900">
+                                {person}
+                              </h3>
+                              <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">
+                                {person}
+                              </span>
+                            </div>
+                            <p className="mt-1 truncate text-sm text-gray-500">
+                              {person}
+                            </p>
+                          </div>
+                          <UserCircleIcon className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" />
+                        </div>
+                        <div>
+                          <div className="-mt-px flex divide-x divide-gray-200">
+                            <div className="flex w-0 flex-1">
+                              <a
+                                href={`mailto:${person}`}
+                                className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                              >
+                                <EnvelopeIcon
+                                  className="h-5 w-5 text-gray-400"
+                                  aria-hidden="true"
+                                />
+                                <span className="ml-3">Email</span>
+                              </a>
+                            </div>
+                            <div className="-ml-px flex w-0 flex-1">
+                              <a
+                                href={`tel:${person}`}
+                                className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                              >
+                                <PhoneIcon
+                                  className="h-5 w-5 text-gray-400"
+                                  aria-hidden="true"
+                                />
+                                <span className="ml-3">Call</span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </>
+
+              <>
+                <div className="mt-4">
+                  <h3 className="text-lg">Your friends</h3>
+                  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    {org.friends.map((org) => (
+                      <div
+                        key={org}
+                        className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+                      >
+                        <Link href={`${org}`}>
+                          <a className="focus:outline-none">
+                            <div className="flex-shrink-0">
+                              <BuildingOfficeIcon className="h-10 w-10 rounded-full" />
+                            </div>
+                            <div className="min-w-0 flex-1">
+                              <span
+                                className="absolute inset-0"
+                                aria-hidden="true"
+                              />
+                              <p className="text-sm font-medium text-gray-900">
+                                {org}
+                              </p>
+                              <p className="truncate text-sm text-gray-500">
+                                {org}
+                              </p>
+                            </div>
+                          </a>
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            </div>
+          )}
         </div>
       </main>
       <div>
@@ -93,7 +203,7 @@ const OrgIndex = ({ userJwt }: { userJwt: string }) => {
   );
 };
 
-function useHandleJoinOrgs(userJwt: string) {
+function useHandleJoinOrg(userJwt: string) {
   const router = useRouter();
   const dispatch = useAppDispatch();
 
