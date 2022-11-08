f9a16cd8c260c58665ba9282db0faa156202f8eb
Mike Alicea <mfalicea58@gmail.com>
2022-11-07 19:15:26 -0500

    Fills out user id

---
+++ src/pages/user/[userId].tsx
@@ -0,0 +1,284 @@
+import { GetServerSideProps } from 'next';
+import { Amplify } from 'aws-amplify';
+import { FunnelIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import cx from 'classnames';
+import { useRouter } from 'next/router';
+
+import { getUserOnServer, AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
+import { useAppDispatch, useAppSelector } from '@/reduxHooks';
+
+import { fetchGetUserById, selectUser } from '@/features/userSlice';
+import { useEffect } from 'react';
+import Image from 'next/image';
+
+// https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
+Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
+
+const team = [
+  {
+    name: 'Leslie Alexander',
+    handle: 'lesliealexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Michael Foster',
+    handle: 'michaelfoster',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Dries Vincent',
+    handle: 'driesvincent',
+    role: 'Manager, Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Lindsay Walton',
+    handle: 'lindsaywalton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+];
+
+const fallbackBgImage =
+  'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80';
+
+interface Props {
+  userJwt: string;
+}
+const UserPage = ({ userJwt }: Props) => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+
+  useEffect(() => {
+    if (router.query.userId) {
+      dispatch(
+        fetchGetUserById({
+          jwtToken: userJwt,
+          userId: router.query.userId as string,
+        })
+      );
+    }
+  }, [dispatch, router.query.userId, userJwt]);
+
+  return (
+    <>
+      <div className="flex h-full">
+        <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+          <div className="relative z-0 flex flex-1 overflow-hidden">
+            <main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+              <article>
+                {/* Profile header */}
+                <div>
+                  <div>
+                    {/* eslint-disable-next-line */}
+                    <img
+                      className="h-32 w-full object-cover lg:h-48"
+                      src={user?.coverImageUrl || fallbackBgImage}
+                      alt=""
+                    />
+                  </div>
+                  <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                    <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                      <div className="flex">
+                        {user?.imageUrl ? (
+                          <Image
+                            className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                            width="24"
+                            height="24"
+                            src={user?.imageUrl || ''}
+                            alt="profile image"
+                          />
+                        ) : (
+                          <span className="inline-flex h-24 w-24 items-center justify-center rounded-full bg-orange-400 sm:h-32 sm:w-32">
+                            <span className="text-3xl font-medium leading-none text-white">
+                              {user?.firstName?.charAt(0)}
+                              {user?.lastName?.charAt(0)}
+                            </span>
+                          </span>
+                        )}
+                      </div>
+                      <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                        <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
+                          <h1 className="truncate text-2xl font-bold text-gray-900">
+                            {user?.firstName} {user?.lastName}
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+                      <h1 className="truncate text-2xl font-bold text-gray-900">
+                        {user?.firstName} {user?.lastName}
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Tabs */}
+                <div className="mt-6 sm:mt-2 2xl:mt-5">
+                  <div className="border-b border-gray-200">
+                    <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                      <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                        <span
+                          className={cx(
+                            'whitespace-nowrap border-b-2 border-pink-500 py-4 px-1 text-sm font-medium text-gray-900'
+                          )}
+                        >
+                          Profile
+                        </span>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Description list */}
+                <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    <div className="sm:col-span-1">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Phone
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        {user?.phoneNumber}
+                      </dd>
+                    </div>
+
+                    <div className="sm:col-span-1">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Email
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        {user?.email}
+                      </dd>
+                    </div>
+
+                    <div className="sm:col-span-1">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Location
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        {user?.location}
+                      </dd>
+                    </div>
+
+                    <div className="sm:col-span-2">
+                      <dt className="text-sm font-medium text-gray-500">
+                        About
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        {user?.description}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+
+                {/* Team member list */}
+                <div className="mx-auto mt-8 max-w-5xl px-4 pb-12 sm:px-6 lg:px-8">
+                  <h2 className="text-sm font-medium text-gray-500">
+                    Team members
+                  </h2>
+                  <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    {team.map((person) => (
+                      <div
+                        key={person.handle}
+                        className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-pink-500 focus-within:ring-offset-2 hover:border-gray-400"
+                      >
+                        <div className="flex-shrink-0">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src={person.imageUrl}
+                            alt=""
+                          />
+                        </div>
+                        <div className="min-w-0 flex-1">
+                          <a href="#" className="focus:outline-none">
+                            <span
+                              className="absolute inset-0"
+                              aria-hidden="true"
+                            />
+                            <p className="text-sm font-medium text-gray-900">
+                              {person.name}
+                            </p>
+                            <p className="truncate text-sm text-gray-500">
+                              {person.role}
+                            </p>
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </article>
+            </main>
+            <aside className="hidden w-96 flex-shrink-0 border-r border-gray-200 xl:order-first xl:flex xl:flex-col">
+              <div className="px-6 pt-6 pb-4">
+                <h2 className="text-lg font-medium text-gray-900">Directory</h2>
+                <p className="mt-1 text-sm text-gray-600">
+                  Search directory of 3,018 employees
+                </p>
+                <form className="mt-6 flex space-x-4" action="#">
+                  <div className="min-w-0 flex-1">
+                    <label htmlFor="search" className="sr-only">
+                      Search
+                    </label>
+                    <div className="relative rounded-md shadow-sm">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                        <MagnifyingGlassIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <input
+                        type="search"
+                        name="search"
+                        id="search"
+                        className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
+                        placeholder="Search"
+                      />
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-3.5 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                  >
+                    <FunnelIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">Search</span>
+                  </button>
+                </form>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const { userJwt } = await getUserOnServer(context);
+
+    return {
+      props: { userJwt },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { userJwt: null, user: null },
+      redirect: {
+        destination: '/login',
+      },
+    };
+  }
+};
+
+export default UserPage;
