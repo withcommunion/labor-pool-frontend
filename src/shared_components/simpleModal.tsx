8a871075fc9f8568aaef6081a2cd550d1c35d40a
Mike Alicea <mfalicea58@gmail.com>
2022-11-11 16:01:29 -0500

    Make modal margin smaller

--- src/shared_components/simpleModal.tsx
+++ src/shared_components/simpleModal.tsx
@@ -58,7 +58,7 @@ export default function SimpleModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative mx-8 transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative mx-2 overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
                   <div className="text-center sm:mt-5">
                     <div className="flex items-center justify-center text-center">
