ccdcb92be8ae87d1c06914cd3c859c6e8f002ad9
Mike Alicea <mfalicea58@gmail.com>
2022-11-15 11:09:21 -0500

    Updates homepage copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Head from 'next/head';
-import {
-  BoltIcon,
-  GlobeAltIcon,
-  ScaleIcon,
-  DocumentChartBarIcon,
-  InboxIcon,
-  PencilSquareIcon,
-  TrashIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline';
+import { BoltIcon, GlobeAltIcon, UsersIcon } from '@heroicons/react/24/outline';
 
 import { Amplify } from 'aws-amplify';
 import { getUserOnServer, AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
@@ -27,60 +18,19 @@ import Image from 'next/image';
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
 const features = [
   {
-    name: `Communion's first feature is a business owner/operator Friends' List and shared worker pool.`,
-    description: `As a business operator, you can create a Friends' List and invite nearby business owners who you trust.
-You and your friends can create a shared worker pool, so that your friends' workers can help cover shifts at your business (and vice-versa)
-`,
+    name: `Find open roles and shifts in the neighborhood`,
+    description: `Imagine a Twitter or Instagram built around finding and giving away open shifts at businesses in the neighborhood. Communion makes it easy to post and find openings.`,
     icon: GlobeAltIcon,
   },
   {
-    name: `Without Communion, you have limited/not-so-good options for covering an open shift:`,
-    description: `Ask another employee to cover it and risk burnout and/or pay overtime
-Cover it yourself and/or send a bunch of texts asking around your personal network
-Use an expensive temp staffing platform like Instawork
-`,
-    icon: ScaleIcon,
-  },
-  {
-    name: 'Communion gives you a better option:',
-    description: `Broadcast the open shift to the shared worker pool, and get one of your friends' workers to cover the shift.`,
-    icon: BoltIcon,
-  },
-  //   {
-  //     name: `Communion takes fairness and anti-poaching seriously:`,
-  //     description: `Give and get: Businesses can broadcast open shifts to the same number of workers they employ. (This means if your business employs 10 workers, and your friends' business employs 5, then they will only be able to broadcast open shifts to 5 of your workers).
-  // Matching wages: Workers who pick up a shift at a friendly restaurant will earn the same rate as at their primary work
-  // Max limit on shift pickups: Workers can pick up a maximum of 15 hours/week at others restaurants
-  // Zero tolerance for poaching: if another business in the network hires one of your workers after having a shift covered, both the business and the worker will be kicked off the platform`,
-  //     icon: ChatBubbleBottomCenterTextIcon,
-  //   },
-];
-
-const howItWorks = [
-  {
-    description: `Let's say you're the owner at Altman's Pizzeria, and you're short a server tomorrow. So, you open the Communion app and broadcasts the open shift.
-`,
-    icon: InboxIcon,
-  },
-  {
-    description: `Alice is a server working at Broadway Diner just down the street, making $18/hr. The diner's owner, Bob, is a friend of yours.
-`,
+    name: `Easily refer friends`,
+    description: `Got a shift to fill? Using Communion to easily share the opening (what, when, where, and requirements) with ex-coworkers, family, and trusted friends.`,
     icon: UsersIcon,
   },
   {
-    description: `Alice opens the Communion app, and sees that there's an open shift at your pizzeria tomorrow. She's available and looking for more hours, so she clicks the Request button in the app.
-`,
-    icon: TrashIcon,
-  },
-  {
-    description: `You get a notification that Alice (who makes $18/hr at Bob's diner) wants to pick up the open shift. Even though you pay your servers $20/hr, because of Communion's safeguards you would pay her $18/hr.
-`,
-    icon: PencilSquareIcon,
-  },
-  {
-    description: `You accept Alice's request, get her contact info from the app, and arranges for her to show up shortly before her new shift.
-`,
-    icon: DocumentChartBarIcon,
+    name: 'Share your time, products, or services with the community',
+    description: `We know many of you who have been in the industry for a long time have a lot to give. Whether it’s management wisdom, hot neighborhood tips, or supplier/service contacts, you can post it on Communion and give back to the community.`,
+    icon: BoltIcon,
   },
 ];
 
@@ -107,20 +57,23 @@ const Index = () => {
       </Head>
 
       <main className="min-h-100vh md:mx-[20vw] ">
-        <div className="flex place-content-around content-center px-4 pt-5 pb-2">
-          <Image
-            src="/images/logo-black.svg"
-            alt="Communion"
-            width="220px"
-            height="62px"
-          />
+        <div className="flex px-4 py-8 pb-2 ">
+          <div className="w-full">
+            <Image
+              className="w-full"
+              src="/images/logo-black.svg"
+              alt="Communion"
+              width="220px"
+              height="62px"
+            />
+          </div>
           <Link
             href={{
               pathname: '/login',
               query: router.query,
             }}
           >
-            <a className="mt-5 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <a className="inline-flex w-fit items-center rounded-md border border-transparent bg-indigo-600 px-8 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               Enter
             </a>
           </Link>
@@ -132,27 +85,35 @@ const Index = () => {
           <div className="w-100vw bg-white py-5">
             <div className="px-4 pb-5 md:mx-[20vw]">
               <p className="z-10 mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-                A better way to stay staffed
+                Welcome friends and participants of BK Food & Hospitality
+                Industry Week!
               </p>
               <p className="mt-4 max-w-2xl text-start text-xl text-gray-500">
-                Communion is an operator- and worker-owned staffing network for
-                retail, restaurants, and hospitality.
+                Communion is a user-owned, social staffing network for people
+                who work in food & hospitality.
                 <br />
                 <br />
-                When you use Communion, you&apos;re awarded points that entitle
-                you to a portion of revenue generated by the platform.
+                For this week (BK Food & Hospitality Industry Week), we’ll be
+                using the app to make it easier for participants to host and
+                attend community-run events and openings.
                 <br />
                 <br />
-                You can think of Communion like an Indeed or Instawork that pays
-                you for being a user and for helping to build the network.
+                Our mission is to break the silos of staffing agencies and
+                platforms, to connect industry people in the neighborhood
+                directly.
+                <br />
+                <br />
+                We believe the solution to some of the industry&apos;s toughest
+                challenges (like understaffing, work culture, or financial
+                safety) can be found just around the corner.
               </p>
             </div>
 
             <div className="w-100vw bg-gray-800">
-              <div className="mx-auto max-w-7xl py-8 px-4 sm:py-12 sm:px-6 lg:flex lg:justify-center lg:px-8">
-                <div className="max-w-xl items-center justify-center">
+              <div className="py-8 px-4 sm:py-12 md:mx-[20vw]">
+                <div className="">
                   <h2 className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">
-                    Interested?
+                    Meet your neighbors and discover new opportunities
                   </h2>
                   <Link
                     href={{
@@ -194,34 +155,28 @@ const Index = () => {
             </div>
           </div>
           {/** /Hero section */}
-
-          {/** How it works */}
-          <div className="w-100vw bg-indigo-700">
-            <div className="max-w-4xl px-4 py-6 sm:px-6 md:mx-[20vw] lg:px-8">
-              <h2 className="text-3xl font-bold tracking-tight text-white">
-                How It Works
-              </h2>
-              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-                {howItWorks.map((feature, idx) => (
-                  <div key={idx}>
-                    <div>
-                      <span className="flex h-12 w-12 items-center justify-center rounded-md bg-white bg-opacity-10">
-                        <p className="text-lg font-bold text-white">
-                          {idx + 1}
-                        </p>
-                      </span>
-                    </div>
-                    <div className="mt-6">
-                      <p className="mt-2 text-base text-indigo-200">
-                        {feature.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
+          <div>
+            <div className="w-100vw bg-indigo-700 py-12">
+              <div className="max-w-4xl px-4 md:mx-[20vw]">
+                <h2 className="text-2xl font-bold tracking-tight text-white">
+                  Communion is in Beta and launching in Brooklyn.
+                  <br />
+                  We&apos;re excited you&apos;re here and would love to hear
+                  what you think!
+                </h2>
+                <Link
+                  href={{
+                    pathname: '/login',
+                    query: router.query,
+                  }}
+                >
+                  <a className="mt-5 inline-flex items-center rounded-md border border-indigo-600 bg-white px-6 py-3 text-center text-base font-medium text-indigo-600 shadow-sm hover:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    Sign Up
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
-          {/** /How it works */}
         </div>
         <div className="my-10">
           <Footer />
